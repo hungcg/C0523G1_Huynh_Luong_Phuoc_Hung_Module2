2d155b8dd2e20c1bcfbeb51e375f0e298afd1684
hungcg <venenozelo94@gmail>
2023-07-04 19:58:34 +0700

    upbt

---
+++ src/ss01_introduce_java/exercise/DocSoThanhChu.java
@@ -0,0 +1,66 @@
+package ss01_introduce_java.exercise;
+
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vô số 4 chữ số thôi nghe :3 nhiều hơn ko biêt code : ");
+        int number = scanner.nextInt();
+        String numberString = String.valueOf(number);
+        String text = "";
+
+        for (int i = 0; i < numberString.length(); ++i) {
+            char x = numberString.charAt(i);
+            if (x != '0') {
+                switch (x) {
+                    case '1':
+                        if (numberString.length() - i == 2) {
+                            text += "mười";
+                        } else {
+                            text += "một ";
+                        }
+                        break;
+                    case '2':
+                        text += "hai ";
+                        break;
+                    case '3':
+                        text += "ba ";
+                        break;
+                    case '4':
+                        text += "bốn ";
+                        break;
+                    case '5':
+                        text += "năm ";
+                        break;
+                    case '6':
+                        text += "sáu ";
+                        break;
+                    case '7':
+                        text += "bảy ";
+                        break;
+                    case '8':
+                        text += "tám ";
+                        break;
+                    case '9':
+                        text += "chín ";
+                }
+
+                switch (numberString.length() - i) {
+                    case 2:
+                        if (numberString.length() - i != 2) {
+                            text += "mươi ";
+                        }
+                        break;
+                    case 3:
+                        text += "trăm ";
+                        break;
+                    case 4:
+                        text += "nghìn ";
+                }
+            }
+        }
+
+        System.out.println(text);
+    }
+}
--- src/ss01_introduce_java/exercise/quy_doi_tien_te.java
+++ src/ss01_introduce_java/exercise/quy_doi_tien_te.java
@@ -10,7 +10,7 @@ public class quy_doi_tien_te {
         Scanner money = new Scanner(System.in);
         System.out.println("nhập số tiền USD bạn muốn quy đổi");
         usd = money.nextDouble();
-        double quydoi = usd * 23000;
+        double quydoi = usd * vnd;
         System.out.println("Số tiền sau khi quy đổi là : " + quydoi + " VND");
     }
 }
